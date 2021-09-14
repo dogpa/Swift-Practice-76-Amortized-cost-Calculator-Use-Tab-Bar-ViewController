ed7272a9c069b9720081736d19c1dc43e97ed2fc
Dogpa <dogpa0130@gmail.com>
2021-09-14 14:01:43 +0800

    Initial Commit

---
+++ Swift Practice # 76 Amortized cost Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 76 Amortized cost Calculator
+//
+//  Created by Dogpa's MBAir M1 on 2021/9/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
